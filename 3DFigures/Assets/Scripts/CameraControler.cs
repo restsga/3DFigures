74d64c84df7c0ff59363ef96fe3c6f7c1e4f1d6e
restsga <restsga@gmail.com>
2018-12-29 13:51:44 +0900

    Add unity project

---
+++ 3DFigures/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+
+    public float speed_xz;
+    public float speed_y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1) {
+            Touch touch = Input.GetTouch(0);
+            this.transform.Rotate(0,touch.deltaPosition.x * speed_xz,0,Space.World);
+            //this.transform.Rotate(0, touch.deltaPosition.y * speed_y, 0, Space.World);
+        }
+    }
+}
